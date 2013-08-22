8e821253be89cb85271d028aae2ddd9ac3de0d4b
Skobayashi <suguru@iii-planning.com>
2013-08-22 17:07:33 +0900

    リンク先をはてブで表示する機能を実装

--- js/background.js
+++ js/background.js
@@ -1,10 +1,20 @@
 
-var context = chrome.contextMenus.create({
+var show_hatebu = chrome.contextMenus.create({
     "title": "このページをはてなブックマークで表示",
     "type": "normal",
     "contexts": ["page"],
     "onclick": function (info, tab) {
-        chrome.tabs.sendRequest(tab.id, {});
+        chrome.tabs.sendRequest(tab.id, {type: 'page'});
+    }
+});
+
+
+var show_entry = chrome.contextMenus.create({
+    "title": "リンク先をはてなブックマークで表示",
+    "type": "normal",
+    "contexts": ["link"],
+    "onclick": function (info, tab) {
+        chrome.tabs.sendRequest(tab.id, {type: 'link', link: info.linkUrl});
     }
 });
 
--- js/content.js
+++ js/content.js
@@ -1,5 +1,18 @@
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    var url;
 
-    var hatebu_url = 'http://b.hatena.ne.jp/entry/'+location.href.replace(/^http(s):\/\//, '');
-    window.open(hatebu_url);
+    // リクエストタイプで処理を振り分ける
+    switch (request.type) {
+        case 'page':
+            // 表示しているページをはてブで表示させる
+            url = 'http://b.hatena.ne.jp/entry/'+location.href.replace(/^http(s):\/\//, '');
+            break;
+
+        case 'link':
+            // 指定リンクをはてブで表示する
+            url = 'http://b.hatena.ne.jp/entry/'+request.link.replace(/^http(s):\/\//g, '');
+            break;
+    }
+
+    window.open(url);
 });
