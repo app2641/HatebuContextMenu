5abb44a1236e76c089b7104505ff9ca013d93bb4
Skobayashi <suguru@iii-planning.com>
2014-05-21 11:14:06 +0900

    HTTPSページに対応するよう修正

--- js/content.js
+++ js/content.js
@@ -5,14 +5,12 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     switch (request.type) {
         case 'page':
             // 表示しているページをはてブで表示させる
-            url = 'http://b.hatena.ne.jp/entry/'+location.href.replace(/^(http|https):\/\//, '');
-            console.log(url);
+            url = 'http://b.hatena.ne.jp/entry/'+location.href;
             break;
 
         case 'link':
             // 指定リンクをはてブで表示する
-            url = 'http://b.hatena.ne.jp/entry/'+request.link.replace(/^(http|https):\/\//, '');
-            console.log(url);
+            url = 'http://b.hatena.ne.jp/entry/'+request.link;
             break;
     }
 
